d30e609c7f04903eb5d500ff975e9e98e1ac1747
VelazqueZzz <m.italy@vk.com>
2023-02-15 22:19:08 +0300

    Add project files.

---
+++ ofstream/ofstream.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, char** argv) {
+    ofstream file("test.txt");
+    if (!file.is_open())return 1;
+    file << "HOLLA HOLA GET A DOLLAR";
+    file.close();
+    return 0;
+}
+ 
+
